9519a1deb929882ad3c28e7c748691e39daa7f17
bgnethmi <nethmiparindya118@gmail.com>
2023-07-31 21:57:48 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,12 +4,15 @@
 
 #define MAX_PASSWORD_LENGTH 60
 
+// Function to generate a random password
 
 void generatePassword(char *pswd, int length) {
 
+//characters to be used in the password
      const char charset[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
 
  int charsetLength = sizeof(charset) - 1;
+//Seed the random number generator
 
  srand(time(NULL));
 
