3889bfe242cda09bf4b7e126d0a7eff015bc6b77
bgnethmi <nethmiparindya118@gmail.com>
2023-07-31 21:56:06 +0530

    Seed the random password

--- main.c
+++ main.c
@@ -7,6 +7,22 @@
 
 void generatePassword(char *pswd, int length) {
 
+     const char charset[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+
+ int charsetLength = sizeof(charset) - 1;
+
+ srand(time(NULL));
+
+    for (int i = 0; i < length; ++i) {
+        int index = rand() % charsetLength;
+        pswd[i] = charset[index];
+    }
+
+    pswd[length] = '\0';
+}
+
+
+
 
 
 
